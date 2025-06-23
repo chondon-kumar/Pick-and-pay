d0ed897ed7e809d639a5ad57c0844637db10758a
chondon_kumar <chondon914@gmail.com>
2025-06-24 01:24:31 +0600

    feat: add database connection and error handling classes

---
+++ Backend/constant.js
@@ -0,0 +1,1 @@
+const DB_NAME = "Pick and Pay";
--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -1,4 +1,24 @@
 import mongoose from "mongoose";
 import { config } from "dotenv";
+import { DB_NAME } from "../../constant";
 
 config()
+
+const connectDB = async ( ) => {
+    if(!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is not defined in the environment variables");
+    }
+    try {
+        const connectionInstance = await mongoose.connect(process.env.MONGO_URI, {
+            dbName: DB_NAME
+        });
+        console.log(`MongoDB connected: ${connectionInstance.connection.host}`);
+        return connectionInstance;
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+        throw error;
+        
+    }
+}
+
+export default connectDB;
