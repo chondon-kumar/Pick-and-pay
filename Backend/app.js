d6c1b8f06b4ec46ca35389f503809d92eae15dc8
chondon_kumar <chondon914@gmail.com>
2025-08-17 15:41:03 +0600

    feat: set up Express server with CORS, cookie parsing, and JSON handling

---
+++ Backend/app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors({
+  origin: process.env.CORS_ORIGIN,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+app.use(express.json({limit: '16kb'})) // for parsing application/json
+app.use(express.urlencoded({extended: true,limit: '16kb'})) // for parsing application/x-www-form-urlencoded
+app.use(express.static('public')) // for serving static files
+app.use(cookieParser()); // for parsing cookies 
+
+
+
+
+export { app };
